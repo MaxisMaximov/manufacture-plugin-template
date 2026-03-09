8e9e28654c9f5f40376294f8fbb6facddec927c1
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2026-03-09 11:51:01 +0100

    Added sample System

--- src/systems.rs
+++ src/systems.rs
@@ -1,3 +1,18 @@
 // # Systems module
 // This is where you can define Systems that will be responsible for running your app
 use super::*;
+use comp::SampleComponent;
+
+pub struct SampleSystem;
+impl System for SampleSystem{
+    type Data<'a> = Query<&'a SampleComponent, ()>;
+    const ID: &'static str = "PositionSystem";
+
+    fn new() -> Self { Self }
+
+    fn execute(&mut self, data: Request<'_, Self::Data<'_>>) {
+        for sample_comp in data.iter(){
+            println!("Sample component val: {}", sample_comp.a)
+        }
+    }
+}

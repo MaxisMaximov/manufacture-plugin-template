7af7798c17c42b0a6af86d01391c352fdfb538c1
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2026-03-09 11:49:38 +0100

    Added sample Component

--- src/comp.rs
+++ src/comp.rs
@@ -1,3 +1,15 @@
 // # Components module
-// This is where you can define Components *(data)* to store data for Entities
+// This is where you can define Components *(data)* for Entities to have
 use super::*;
+use storage::SampleStorage;
+
+#[derive(Clone, Copy)]
+pub struct SampleComponent{
+    pub a: u8,
+    pub b: Vector2
+}
+impl Component for SampleComponent{
+    type STORAGE = SampleStorage<Self>;
+
+    const ID: &'static str = "SampleComponent";
+}

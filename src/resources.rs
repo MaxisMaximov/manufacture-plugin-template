6af7780cc8bed39ea3e8a4d4972ef7e8f33ddf1e
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2026-03-09 11:49:50 +0100

    Added sample Resource

--- src/resources.rs
+++ src/resources.rs
@@ -1,3 +1,18 @@
 // # Resource module
 // This is where you can define global Resources to be used by Systems
 use super::*;
+
+pub struct SampleResource{
+    pub a: Vector2,
+    pub b: Vector2
+}
+impl Resource for SampleResource{
+    const ID: &'static str = "SampleResource";
+
+    fn new() -> Self {
+        Self{
+            a: Vector2 { x: 5.0, y: 5.0 },
+            b: Vector2 { x: 10.0, y: 10.0 },
+        }
+    }
+}

e69acf3833aa2a893b24910a0fcf39c6873dfdff
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2026-03-09 11:50:50 +0100

    Added sample Event

--- src/events.rs
+++ src/events.rs
@@ -1,3 +1,8 @@
 // # Events module
 // This is where you can define custom Events to send announcements to other Systems and trigger Event Responders
 use super::*;
+
+pub struct SampleEvent(pub &'static str);
+impl Event for SampleEvent{
+    const ID: &'static str = "SampleEvent";
+}

640b1328ec96ed3bbbb8d6fde18aa4c611421e13
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2026-03-09 11:51:23 +0100

    Updated `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 // # Plugin library
 // The main library file where you define modules within your plugin, as well as where the plugin is initialized at runtime
 use manufacture_core::prelude::*;
+use manufacture_engine::prelude::*;
+use manufacture_engine::ECS::dispatcher::DispatcherBuilder;
 
 pub mod commands;
 pub mod comp;
@@ -15,7 +17,11 @@ pub mod systems;
 /// ## DO NOT MODIFY THIS FUNCTION'S SIGNATURE
 /// The Plugin Loader will not recognize your plugin if this function is changed in any way
 /// 
-/// You can add and remove things to initialize, but **do not** change the function's name or inputs
+/// You can add and remove things to initialize, but **do not** change the function's signature
 pub fn init_plugin(world: &mut World, disp_build: &mut DispatcherBuilder){
+    world.register_comp::<comp::SampleComponent>();
+    world.register_res::<resources::SampleResource>();
+    world.register_event::<events::SampleEvent>();
 
+    disp_build.add::<systems::SampleSystem>();
 }

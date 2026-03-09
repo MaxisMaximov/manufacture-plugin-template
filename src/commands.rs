02ac39ec1641493e06464aabc557e9462c3b13f6
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2026-03-09 11:50:03 +0100

    Added sample Command

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,10 @@
 // # Commands module
 // This is where you can define Commands that perform operations on World at root level
 use super::*;
+
+pub struct SampleCommand(usize);
+impl Command for SampleCommand{
+    fn execute(&mut self, world: &mut World) {
+        world.despawn(self.0);
+    }
+}
